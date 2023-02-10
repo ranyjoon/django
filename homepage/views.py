2070d34e58052927dbf1b998abfa6527e06ff481
ranyjoon <joony.yoon@gmail.com>
2023-02-10 12:39:13 +0900

    'first'

---
+++ homepage/views.py
@@ -0,0 +1,13 @@
+from django.shortcuts import render
+
+def landing(request):
+    return render(
+        request,
+        'homepage/landing.html'
+    )
+
+def about_me(request):
+    return render(
+        request,
+        'homepage/about_me.html'
+    )
